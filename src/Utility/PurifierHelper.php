870cdc4d31ba1003c9f27295d05779c66c07c927
Pete Burkindine <peter.burkindine@directenergy.com>
2017-03-10 19:15:18 -0600

    Add purifier destroyer to make the object easier to log

--- src/Utility/PurifierHelper.php
+++ src/Utility/PurifierHelper.php
@@ -28,4 +28,15 @@ trait PurifierHelper
 
         return $purifier;
     }
+
+    /**
+     * destroyPurifier()
+     *
+     * When the purifier is no longer needed by your application, you can call ::destroyPurifier on the objects
+     * to set it null.  This makes the object easier to log.
+     */
+    protected function destroyPurifier()
+    {
+        $this->purifier = null;
+    }
 }
